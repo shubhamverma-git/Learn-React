4ba25178cd4b1f0fcba98f01188bd34a114f4bf9
Shubham Verma <vermashubham2707@gmail.com>
2023-12-14 18:00:51 +0530

    -- counter

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -1,7 +1,33 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">HII</div>;
+  let [counter, setCounter] = useState(0);
+  // const [dec, setDec] = useState(0);
+
+  const addValue = () => {
+    counter = counter + 1;
+    setCounter(counter);
+    if (counter > 20) {
+      alert(`can't be more then twenty`);
+    }
+  };
+
+  const decValue = () => {
+    counter = counter - 1;
+    setCounter(counter);
+    if (counter < 0) {
+      alert(`can't be less then zero`);
+    }
+  };
+  return (
+    <div className="App">
+      <h1>React Project From Basic</h1>
+      <h2>Counter Value: {counter}</h2>
+      <button onClick={addValue}>Add Value</button>
+      <button onClick={decValue}>Decrease Value</button>
+    </div>
+  );
 }
 
 export default App;
