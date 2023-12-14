da739a0bb64e201411c3d7757f0cc84293461a82
Shubham Verma <vermashubham2707@gmail.com>
2023-12-15 02:29:16 +0530

    -- use of react hooks

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -1,71 +1,102 @@
-import { useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import "./App.css";
-import Card from "./Components/Card";
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  const [color, setColor] = useState("white");
+  const [length, setLength] = useState(8);
+  const [number, setNumber] = useState(false);
+  const [characters, setCharacters] = useState(false);
+  const [password, setPassword] = useState("");
 
-  let myObj = {
-    Name: "Mianain",
-    Age: 35,
-    City: "None",
-  };
+  // useCallback is used to memoize the function in the cache between re-renders
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (number) str += "0987654321";
+    if (characters) str += "`!@#$%^&*(),.<>'|[]{}+=";
 
-  const addValue = () => {
-    counter = counter + 1;
-    setCounter(counter);
-    if (counter > 20) {
-      alert(`can't be more then twenty`);
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
     }
-  };
+    setPassword(pass);
+  }, [length, number, characters, setPassword]);
 
-  const decValue = () => {
-    counter = counter - 1;
-    setCounter(counter);
-    if (counter < 0) {
-      alert(`can't be less then zero`);
-    }
-  };
+  //useEffect to run the function whenever the page render
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, number, characters, passwordGenerator]);
+
+  //useRef used to take reference. Here we are using to take reference of the input field data
+  const passwordRef = useRef(null);
+
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 28); // to select only 20 letters
+    window.navigator.clipboard.writeText(password);
+  }, [password]);
+
   return (
-    <div style={{ backgroundColor: color, height: "100vh" }} className="App">
-      <h1 className="bg-green-500 text-orange p-4">React Project From Basic</h1>
-      <h2>Counter Value: {counter}</h2>
-      <button onClick={addValue}>Add Value</button>
-      <button onClick={decValue}>Decrease Value</button>
-      {/* <Card country="India" position="Developer" name="He" btnText="Visit Me" /> */}
-      {/* <Card country="none" position="Manager" name="She" someObj={myObj} /> */}
-      {/* <Card country="one" position="Senior" name="We" btnText="Know More" /> */}
-
-      <div className="container">
-        {btnName.map((i, idx) => (
-          <div key={idx} className="btn_container">
-            <button
-              className="btn"
-              onClick={() => setColor(i.chclr)}
-              style={{ backgroundColor: i.bgclr, color: i.clr }}
-            >
-              {i.name}
-            </button>
+    <div className="App">
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg p-4 my-82  text-orange-500 bg-gray-800">
+        <h1 className="text-center text-white">Password Generator</h1>
+        <div className="flex shadow mt-10 rounded-lg overflow-hidden mb-4">
+          <input
+            value={password}
+            className="outline-none w-full py-1 px-3 text-black"
+            type="text"
+            placeholder="Password"
+            readOnly
+            ref={passwordRef}
+          />
+          <button
+            onClick={copyPasswordToClipboard}
+            className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0"
+          >
+            Copy
+          </button>
+        </div>
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-1">
+            <input
+              type="range"
+              min={6}
+              max={100}
+              value={length}
+              className="cursor-pointer"
+              onChange={(e) => {
+                setLength(e.target.value);
+              }}
+            />
+            <label>Length : {length}</label>
           </div>
-        ))}
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={number}
+              className="cursor-pointer"
+              id="numberInput"
+              onChange={() => {
+                setNumber((prev) => !prev);
+              }}
+            />
+            <label htmlFor="">Number</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={characters}
+              className="cursor-pointer"
+              id="numberInput"
+              onChange={() => {
+                setCharacters((prev) => !prev);
+              }}
+            />
+            <label htmlFor="">Characters</label>
+          </div>
+        </div>
       </div>
     </div>
   );
 }
 
 export default App;
-
-const btnName = [
-  { bgclr: "Red", chclr: "Red", name: "Red" },
-  { bgclr: "Green", chclr: "Green", name: "Green" },
-  { bgclr: "Olive", chclr: "Olive", name: "Olive" },
-  { bgclr: "Gray", chclr: "Gray", name: "Gray" },
-  { bgclr: "Yellow", clr: " black", chclr: "Yellow", name: "Yellow" },
-  { bgclr: "Pink", chclr: "Pink", name: "Pink" },
-  { bgclr: "Purple", chclr: "Purple", name: "Purple" },
-  { bgclr: "Lavender", clr: " black", chclr: "Lavender", name: "Lavender" },
-  { bgclr: "White", clr: " black", chclr: "White", name: "White" },
-  { bgclr: "Black", chclr: "Black", name: "Black" },
-  { bgclr: "Orange", chclr: "Orange", name: "Orange" },
-];
